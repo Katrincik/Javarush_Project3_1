9d327d48b05131548ce5f506b98c6fd1d385b0b0
Katrincik <grebennicova.ecaterina@gmail.com>
2026-03-23 13:36:40 +0200

    chore: create the deploy workflow

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/Javarush_Project3_1/', // GitHub Pages repo name; use '/' if using a custom domain
   plugins: [
     react({
       babel: {
